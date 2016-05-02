90e7054b9ffe089be38ff4a3f5272b1bd296ca43
Christian <Christian@82.211.219.37>
2016-05-02 08:33:43 +0200

    Commiting test files for SetSickness and CreateAssignment

---
+++ SE1 - project/src/Tests/CreateAssignmentTest.java
@@ -0,0 +1,184 @@
+package Tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Assert;
+
+import org.junit.Test;
+
+import Exceptions_Errors.*;
+import project.*;
+
+
+/**
+ * Tests for user case createAssignment
+ * @author Christian 
+ */
+
+public class CreateAssignmentTest extends SampleDataSetup0 {
+		/*
+		 *Main scenario: 
+		 * employee is project leader
+		 * employee enters taskID and EmployeeID
+		 * system creates an assignment
+		 * 		check assignment exists
+		 * 		check if its the right employee
+		 * 		check if its the right task
+		 */
+		
+		@Test
+		public void testCreateAssignmentMain () {
+			Employee employee=super.database.employees.get(0);
+			Project project=super.database.projects.get(0);
+			
+			assertTrue(project.isProjectLeader(employee));
+			
+			String taskName="Task01";
+			int taskID=1;
+			
+			taskID = employee.createTask(database, project, taskName); //Jeg vil gerne have det her udenfor
+			
+			String assignmentName="Assignment for the task "+taskName+" for the employee "+employee;
+			int assignmentID=-1;
+			
+			assertFalse(employee.isAssigned(taskID)); //Skal laves
+			
+			try {
+				assignmentID = employee.createAssignment(database, taskName, employee); //skal laves
+			} catch (WrongInputException e) {
+			}
+			
+			Assignment assignment=super.database.getTask(assignmentID); //Ved ikke om vi skal indfører et ID for dette?
+			
+			assertNotNull(assignment); //Kom gerne med bedre forslag til at tjekke disse ting
+			assertEquals(assignment.name,assignmentName);
+			assertEquals(assignment.taskID,task);
+			assertEquals(assignment.employee,employeeID); //Hvorfor gør den dette? Jeg er bange... :(
+		}
+		
+		/*
+		 *Alternative scenario1: 
+		 * employee is not project leader
+		 * employee tries to create task for project
+		 * exception is thrown
+		 * no task has been created
+		 */
+		
+		@Test
+		public void testCreateTaskAlt1(){
+			int numberOfAssignments = database.getNumberOfAssignments(); //Kommer vi til at indfører antal?
+			Employee employee=super.database.employees.get(1);
+			Project project=super.database.projects.get(0);
+			
+			//checks employee is not project leader
+			assertFalse(project.isProjectLeader(employee));
+			
+			String taskName="Task01";
+			employee.createTask(database, project, taskName);
+			
+			try {
+				assignmentID = employee.createAssignment(database, taskName, employee);
+				Assert.fail(); //checks exception is thrown
+			} catch (WrongInputException e) {
+			}
+			
+			//checks createTask hasn't created task; 
+			assertEquals(database.getNumberOfAssignments(), numberOfAssignments);
+		}
+
+		/*
+		 *Alternative scenario2: 
+		 * employee tries to create a assignment
+		 * but the task and employee is already assigned
+		 * exception is thrown
+		 * no task has been created
+		 */
+		
+		@Test
+		public void testCreateTaskAlt2(){
+			int numberOfAssignments = database.getNumberOfAssignments();
+			Employee employee=super.database.employees.get(0);
+			Project project=super.database.projects.get(0);
+			
+			assertTrue(project.isProjectLeader(employee));
+			
+			String taskName="Task01";
+			int taskID=1;
+			
+			taskID = employee.createTask(database, project, taskName); 
+			
+			String assignmentName="Assignment for the task "+taskName+" for the employee "+employee;
+			int assignmentID=-1;
+			
+			assertTrue(employee.isAssigned(taskID));
+			
+			try {
+				assignmentID = employee.createAssignment(database, taskName, employee);
+				Assert.fail(); //checks exception is thrown
+			} catch (WrongInputException e) {
+			}
+			
+			//checks createTask hasn't created task; 
+			assertEquals(database.getNumberOfAssignments(), numberOfAssignments);
+			
+		}
+		
+		/*
+		 *Alternative scenario3:
+		 * employee tries to create assignment for non excisting task (null)
+		 * method throws exception
+		 * no task has been added
+		 */
+		
+		@Test
+		public void testCreateTaskAlt3(){
+			int numberOfAssignments = database.getNumberOfAssignments();
+			Employee employee=null;
+			
+			
+			String taskName=null;
+			
+			try {
+				employee.createAssignment(database, taskName, employee);
+				Assert.fail(); //method throws exception
+			} catch (WrongInputException e) { 
+			}
+			
+			//checks createTask hasn't created task; 
+			assertEquals(database.getNumberOfAssignments(), numberOfAssignments);
+		}
+		
+		/*
+		 *Alternative scenario4: 
+		 * employee tries to create assignment for non excisting employee (null)
+		 * method throws exception
+		 * no task has been added
+		 */
+		
+		@Test
+		public void testCreateTaskAlt4(){
+			int numberOfAssignments = database.getNumberOfAssignments();
+			Employee employee=super.database.employees.get(0);
+			
+			
+			String taskName="Task01";
+			
+			try {
+				employee.createAssignment(database, taskName, employee);
+				Assert.fail(); //method throws exception
+			} catch (WrongInputException e) { 
+			}
+			
+			//checks createTask hasn't created task; 
+			assertEquals(database.getNumberOfAssignments(), numberOfAssignments);
+		}
+		
+		
+		
+		
+}
+
+
+
---
+++ SE1 - project/src/Tests/SetSickness.java
@@ -0,0 +1,65 @@
+package Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Exceptions_Errors.WrongInputException;
+import project.Employee;
+
+/**
+ * Tests for user case setSickness
+ * @author Christian 
+ */
+
+public class SetSickness extends SampleDataSetup0 {
+	
+	/*
+	 *Main scenario: 
+	 * employee wants to create booking for sickness
+	 * employee uses the method setSickness
+	 * system creates set employee as sick
+	 * 		check if employee already is sick
+	 * 		check if employee is booked as sick
+	 */
+	
+	@Test
+	public void testSetSicknessMain () {
+		Employee employee=super.database.employees.get(1);
+		
+		int sickness=0;
+		
+		assertFalse(employee.isSick(employee)); //metoden skal laves
+		
+		try { //tror heller ikke at dette er nødvendigt
+			sickness = employee.setSickness(employee /*er i tvivl om den skal bruges her*/); //skal laves
+		} catch (WrongInputException e) {
+		}
+		
+		//checks if employee is sick
+		assertTrue(employee.isSick(employee));
+	}
+	
+	/*
+	 *Alternative scenario1: 
+	 * employee is not project leader
+	 * employee tries to create task for project
+	 * exception is thrown
+	 * no task has been created
+	 */
+	
+	@Test
+	public void testCreateTaskAlt1(){
+Employee employee=super.database.employees.get(1);
+		
+		int sickness=1;
+		
+		assertTrue(employee.isSick(employee));
+		
+		try { 
+			sickness = employee.setSickness(employee);
+			Assert.fail(); //checks if exception is thrown
+		} catch (WrongInputException e) { //exception skal laves
+		}
+		
+	}
+}
